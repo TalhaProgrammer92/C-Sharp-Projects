3266a6496427766556a58924d95642fa49b0554a
TalhaProgrammer92 <talha.code92@gmail.com>
2025-04-28 09:55:22 +0500

    Added Chess Project

---
+++ Console/Chess/Player.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess
+{
+    class Player
+    {
+        // Attributes
+        public string name { get; }
+        public Score score;
+        public int won_match, lose_match;
+
+        // Constructor
+        public Player(string name)
+        {
+            this.name = name;
+            score = new Score();
+
+            won_match = 0;
+            lose_match = 0;
+        }
+
+        // Display stats
+        public void displayStats()
+        {
+            // Heading
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"*** {name} Stats ***");
+
+            // Stats
+            Console.ForegroundColor= ConsoleColor.Cyan;
+            Console.WriteLine($"\nScore:\t{score.getScore()}\nWon:\t{won_match}\nLose:\t{lose_match}");
+            
+            Console.ResetColor();
+        }
+    }
+}
---
+++ Console/Chess/Score.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess
+{
+    class Score
+    {
+        // Attributes
+        private int score_count = 0;
+
+        // Score getter
+        public int getScore()
+        {
+            return score_count;
+        }
+
+        // Score update
+        public void updateScore(int score)
+        {
+            score_count += score;
+        }
+
+        // Score reset
+        public void resetScore()
+        {
+            score_count = 0;
+        }
+    }
+}
